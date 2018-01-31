9e4fde12b15646167ad02f8e0b1ec4ab4b99c7ee
BorisSiman <boris.siman@devtechgroup.com>
2018-01-31 16:25:37 +0100

    added js to quiz

---
+++ question.js
@@ -0,0 +1,61 @@
+var questions = [
+    {
+        "question" : "Whats the capital of Columbia?",
+        "answer1" : "Cairo",
+        "answer2" : "Bogota",
+        "answer3" : "Baku",
+        "answer4" : "Praia",
+        
+        "answer" : 2
+    },
+    
+    {
+        "question" : "Whats the capital of Bolivia?",
+        "answer1" : "Sofia",
+        "answer2" : "San Jose",
+        "answer3" : "Bengui",
+        "answer4" : "La Paz",
+        
+        "answer" : 4
+    },
+    
+    {
+        "question" : "Whats the capital of Iran",
+        "answer1" : "Tehran",
+        "answer2" : "Baghdad",
+        "answer3" : "Tel Aviv",
+        "answer4" : "Astana",
+        
+        "answer" : 1
+    },
+    
+    {
+        "question" : "Whats the capital of Georgia",
+        "answer1" : "Accra",
+        "answer2" : "Tbilisi",
+        "answer3" : "Male",
+        "answer4" : "Wellington",
+        
+        "answer" : 2
+    },
+    
+    {
+        "question" : "Whats the capital of Nepal?",
+        "answer1" : "Rabat",
+        "answer2" : "Abuja",
+        "answer3" : "Kathmandu",
+        "answer4" : "Belgrade",
+        
+        "answer" : 3
+    },
+    
+    {
+        "question" : "Whats the capital of Niger?",
+        "answer1" : "Niamey",
+        "answer2" : "Belfast",
+        "answer3" : "Chisinau",
+        "answer4" : "Dakar",
+        
+        "answer" : 1
+    }
+];
---
+++ quiz-script.js
@@ -0,0 +1,73 @@
+var score = 0;
+var questionNum = 0;
+var totalQuestions = questions.length;
+
+var questionSite = document.getElementById("question");
+var ans1 = document.getElementById("ans1");
+var ans2 = document.getElementById("ans2");
+var ans3 = document.getElementById("ans3");
+var ans4 = document.getElementById("ans4");
+var scoreSite = document.getElementById("score");
+var scoreFinal = document.getElementById("finalPage");
+
+var nextButton = document.getElementById("nextButton");
+var restartButton = document.getElementById("goAgain");
+var finalButton = document.getElementById("finalScore");
+
+restartButton.style.display = 'none';
+finalButton.style.display = 'none';
+
+function questionLoad(questNum){
+    var q = questions[questNum];
+    questionSite.textContent = (questNum + 1) + '.' + q.question;
+    ans1.textContent = q.answer1;
+    ans2.textContent = q.answer2;
+    ans3.textContent = q.answer3;
+    ans4.textContent = q.answer4;
+    scoreSite.textContent = (questionNum+1) + ' of ' + totalQuestions;
+};
+
+function loadNext(){
+    function restart(){
+        score = 0;
+        questionNum = 0;
+        nextButton.style.display = '';
+        nextButton.textContent = 'Next';
+        restartButton.style.display = 'none';
+        finalButton.style.display = 'none';
+        questionLoad(questionNum);
+    }
+    
+    function finalScore(){
+        window.location.href = "score.html";
+        scoreFinal.textContent = "Your score is " + score + " of " + totalQuestions + "that is : " + ((totalQuestions/score)*100) + "%";
+    }
+
+    var selected = document.querySelector('input[type=radio]:checked');
+    if(!selected){
+        alert('Please check your answer!')
+        return;
+    }
+    var answer = selected.value;
+    if(questions[questionNum].answer == answer){
+        score +=1;
+    }
+    selected.checked = false;
+    questionNum++;
+    if(questionNum == totalQuestions - 1){
+        nextButton.textContent = 'Finish';
+    }
+    if(questionNum == totalQuestions){
+        scoreSite = alert("Your score is: " + score + "!\nClick on Final score button to check your answers and quiz procent or click restart button to try again!");
+        restartButton.style.display = '';
+        finalButton.style.display = '';
+        nextButton.style.display = 'none';
+        restartButton.addEventListener("click", restart);
+        finalButton.addEventListener("click", finalScore);
+        return;
+    }
+    
+    questionLoad(questionNum);
+}
+
+questionLoad(questionNum);
